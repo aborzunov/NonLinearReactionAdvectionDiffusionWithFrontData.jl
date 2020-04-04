10c38d79aff50c9dbea0eb05dd36c6b3f9c53e01
Andrey Borzunov <Andrey.Borzunov@gmail.com>
2020-04-04 16:08:41 +0300

    Изменение сигнатур heterogenety, дельта-функций.
    Подправлены тесты градиента `test/gradient_check.jl`.

--- test/gradient_check.jl
+++ test/gradient_check.jl
@@ -1,50 +1,41 @@
-@testset "Градиент на точных данных" begin
+using  NonLinearReactionAdvectionDiffusionWithFrontData: heterogenety
+
+@testset "Градиент на точных данных и статической сетке    " begin
 
     # Создадим априорную информацию и точное решение для точного q(x)
+    a, b, t₀, T, N, M, ε, Xₙ, Tₘ, qₙ, ulₘ, urₘ, u₀ = NonLinearReactionAdvectionDiffusionWithFrontData.dparams();
+    #
+    u, XX, TP = solve(u₀, Xₙ, N, Tₘ, M, ε, ulₘ, urₘ, qₙ);
     #########################################################################################
-    u_l(t) = -8.0
-    u_r(t) =  4.0
-    q(x) = 4*sin(3 * π * x);        # Коэффициент линейного усиления, который в обратной
-    # задаче необходимо определить, но при генерации априорной
-    # информации мы задаем некоторый коэффициент, который,
-    # собственно, после имея априорную информацию и будем определять.
-    ε = 0.2;                        # Малый параметр при старшей производной
-    a, b = 0, 1;                    # Область по X
-    t₀, T = 0, 0.36;                # Область по T
-    N, M = 100, 80;                 # Кол-во разбиений по X, T
-    h = (b-a)/N;                    # шаг по X
-    τ = (T-t₀)/M;                   # шаг по T
-    Xₙ = [a  + n*h for n in 0:N];   # Сетка по Х
-    Tₘ = [t₀ + m*τ for m in 0:M];   # Сетка по Т
-    qₙ =      q.(Xₙ);               # Сеточные значения коэффициента линейного усиления
-    ulₘ=    u_l.(Tₘ);               # Сеточные значения левого  ГУ
-    urₘ=    u_r.(Tₘ);               # Сеточные значения правого ГУ
-    u₀ = u_init.(Xₙ);               # Начальные условия
+    ϕl      = phidetermination(qₙ, ulₘ, Xₙ, N, Tₘ, M);                      # Левый вырожденный корень
+    ϕr      = phidetermination(reverse(qₙ), urₘ, reverse(Xₙ), N, Tₘ, M);    # Нужно подать инвертированную сетку
+    ϕr      = reverse(ϕr, dims=1);                                          # А после — инвертировать решение по X
+    ϕ       = Φ(ϕl, ϕr, N, M);                                              # Серединный корень
+    f1_data = f1(ϕ, u, Xₙ, N, M);                                           # Положение переходного слоя
+    f2_data = f2(f1_data, u, Xₙ, N, M);                                     # Значение функции на переходном слое
+    #########################################################################################
     nothing #hide
 
-    u = solve(u₀, Xₙ, N, Tₘ, M, ε, ulₘ, urₘ, qₙ);
-    ϕl = phidetermination(qₙ, ulₘ, Xₙ, N, Tₘ, M);                               # Левый вырожденный корень
-    ϕr = phidetermination(qₙ, urₘ, reverse(Xₙ), N, Tₘ, M);                      # Нужно подать инвертированную сетку
-    ϕr = reverse(ϕr, dims=1);                                                   # А после — инвертировать решение по X
-    ϕ = NonLinearReactionAdvectionDiffusionWithFrontData.Φ(ϕl, ϕr, N, M);       # Серединный корень
-    f1 = NonLinearReactionAdvectionDiffusionWithFrontData.f1(ϕ, u, Xₙ, N, M);   # Положение переходного слоя
-    f2 = NonLinearReactionAdvectionDiffusionWithFrontData.f2(f1, u, Xₙ, N, M);  # Значение функции на переходном слое
-    nothing #hide
-    #########################################################################################
-
-    using  NonLinearReactionAdvectionDiffusionWithFrontData: heterogenety
     # Функция `heterogenety` — для внутреннего использования,
     # она принимает аргументы без граничных точек по x.
     Uₙₘ = u[2:N, :];
-    X = Xₙ[2:N];
-    H = [  - heterogenety(n, m, X, N, Tₘ, M, Uₙₘ, f1, f2) for n in 1:N-1, m in 1:M+1];
+    X = XX[2:N, :];
+    # Создадим замыкания, для удобства
+    h(z) = [  - heterogenety(n, m, X[:, m], N, Tₘ, M, Uₙₘ, f1_data, f2_data, w=z) for n in 1:N-1, m in 1:M+1]
+    nnz(arr) = length( findall( x -> ! isapprox(x, 0), arr ))
+    H = h(0.001);    # Значения неоднородности на сетке `` Xₙ × Tₘ``
 
-    # Количество ненулевых элементов меньше 25%
-    @test length( findall( x -> x!= 0, H) ) < M/4
+    # Количество ненулевых элементов — 0
+    @test       nnz(h(0.0001)) < M/4        # 0, Не рекомендовано
+    @test 0 < nnz(h(0.0004)) < M/10    # 5, норм!
 
-    # Для визуального контроля, можно использовать следующий код
+    # Для визуального контроля, можно использовать следующий код дающий анимацию
     #=
-    using Plots, LaTeXStrings
+    # {{{
+    using Plots, LaTeXStrings;
+    pyplot();
+
+    heatmap(h(0.0004)', label=L"- 2 \delta(x - f_1(t)) ( u^s(x,t) - f_2(t))")
 
     d = [ deltaw( n, f1[m], Xₙ[2:N], N) for n in 1:N-1, m in 1:M+1];    # Значения первого множителя
     uf = [ ( Uₙₘ[n+1, m] - f2[m] ) for n in 1:N-1, m in 1:M+1];         # Значения второго множителя
@@ -67,5 +58,39 @@
     # Карта поверхности неоднородности
     heatmap(X, Tₘ, H',  xlabel = "x", ylabel="t", title=L"- 2 \delta(x - f_1(t)) ( u^s(x,t) - f_2(t))")
     savefig("heterogenety.png")
+    # }}}
     =#
+
+end
+
+@testset "Градиент на точных данных и динамической сетке   " begin
+
+
+    a, b, t₀, T, N, M, ε, Xₙ, Tₘ, qₙ, ulₘ, urₘ, u₀, mshfrm = NonLinearReactionAdvectionDiffusionWithFrontData.dparams_nonuniform();
+    #
+    u, XX, TP = solve(u₀, Xₙ, N, Tₘ, M, ε, ulₘ, urₘ, qₙ, create_mesh = mshfrm);
+    #' ## Генерация априорной информации
+    ϕl = phidetermination(qₙ, ulₘ, Xₙ, N, Tₘ, M);                               # Левый вырожденный корень
+    ϕr = phidetermination(reverse(qₙ), urₘ, reverse(Xₙ), N, Tₘ, M);             # Нужно подать инвертированную сетку
+    ϕr = reverse(ϕr, dims=1);                                                   # А после — инвертировать решение по X
+    ϕ = Φ(ϕl, ϕr, N, M);                                                        # Полуразность вырожденных корней
+    ϕ = apply_on_dynamic_mesh(ϕ, XX, N, M);                                     # Аппроксимация на переменную сетку
+    ϕl = apply_on_dynamic_mesh(ϕl, XX, N, M);                                   # Аппроксимация на переменную сетку
+    ϕr = apply_on_dynamic_mesh(ϕr, XX, N, M);                                   # Аппроксимация на переменную сетку
+    f1_data = f1(ϕ, u, XX, N, M);                                               # Положение переходного слоя
+    f2_data = f2(f1_data, u, XX, N, M);                                         # Значение функции на переходном слое
+
+
+    using  NonLinearReactionAdvectionDiffusionWithFrontData: heterogenety
+    # Функция `heterogenety` — для внутреннего использования,
+    # она принимает аргументы без граничных точек по x.
+    Uₙₘ = u[2:N, :];
+    X = XX[2:N, :];
+    h(z) = [  - heterogenety(n, m, X[:, m], N, Tₘ, M, Uₙₘ, f1_data, f2_data, w=z) for n in 1:N-1, m in 1:M+1]
+    nnz(arr) = length( findall( x -> ! isapprox(x, 0), arr ));
+
+    # Количество ненулевых элементов — 0
+    @test nnz(h(0.00001)) < M/4        # 0, Не рекомендовано
+    @test 0 < nnz(h(0.000005)) < M/10    # 5, норм!
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,7 @@
-using NonLinearReactionAdvectionDiffusionWithFrontData
+using NonLinearReactionAdvectionDiffusionWithFrontData;
+using NonLinearReactionAdvectionDiffusionWithFrontData: f1, f2;
+using NonLinearReactionAdvectionDiffusionWithFrontData: apply_on_dynamic_mesh, Φ;
+
 using Test
 using Documenter
 using ForwardDiff
@@ -11,6 +14,8 @@ end
 include("utils.jl")
 
 include("direct_check.jl")
-include("adjoint_check.jl")
-include("gradient_check.jl")
-include("degenerated_check.jl")
+include("direct_nonuniform_check.jl")
+
+#include("adjoint_check.jl")
+#include("gradient_check.jl")
+#include("degenerated_check.jl")
