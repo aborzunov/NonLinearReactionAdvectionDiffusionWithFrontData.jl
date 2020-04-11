bb0c36d678cc811762f69631a798eeb038003619
Andrey Borzunov <Andrey.Borzunov@gmail.com>
2020-04-11 14:11:55 +0300

    Фикс тестов, переименовал q(x) -> qf(x)

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Documenter
 using ForwardDiff
 using LinearAlgebra
 
-include("utils.jl")
+@time @testset "Вспомогательные                                  " begin include("utils.jl") end
 
 @time @testset "Якобиан прямой задачи на статической сетке.      " begin include("direct_jacobian.jl") end
 @time @testset "Якобиан прямой задачи на динамической сетке.     " begin include("direct_jacobian_nonuniform.jl") end
@@ -15,6 +15,7 @@ include("utils.jl")
 @time @testset "Модельная невязка для сопряженной задачи         " begin include("adjoint_check.jl") end
 
 @time @testset "Неоднородность на точных данных                  " begin include("heterogeneity_check.jl") end
+@time @testset "Новая аппроксимация дельта-функции               " begin include("delta_check.jl") end
 @time @testset "Вырожденые корни                                 " begin include("degenerated_check.jl") end
 #
 @time @testset "Градиент, функционал                             " begin include("functional.jl") end
--- test/utils.jl
+++ test/utils.jl
@@ -1,4 +1,4 @@
-@testset "Utils                                            " begin
+@testset "Различные вспомогательные функции                " begin
 
     N = 20;
     X = [i/N for i in 0:N];
@@ -19,39 +19,6 @@
         @test_throws AssertionError NonLinearReactionAdvectionDiffusionWithFrontData.strip_borderPoints(B, N)
     end
 
-    @testset "`delta` function" begin
-        d = [ delta(x, X, 0.5) for x in X ]
-
-        @testset "формула прямоугольников" begin
-        I = 0.0;
-            for n in 1:N
-                δx = X[n+1] - X[n]
-                I += d[n] * δx
-            end
-            @test isapprox(I, 1.0)
-        end
-
-        @testset "формула трапеций" begin
-        I = 0.0;
-            for n in 1:N
-                δx = X[n+1] - X[n]
-                I += ((d[n] + d[n+1]) / 2.0) * δx
-            end
-            @test isapprox(I, 1.0)
-        end
-
-        # Возвращает только один ненулевой элемент
-        @test length( filter( x -> x != 0, d) ) == 1
-
-        @testset "Проверка области определения дельта-функции" begin
-            @test_throws DomainError delta(-2, X, 0.5)
-            @test_throws DomainError delta(2, X, 0.5)
-            @test_throws DomainError delta(0.5, X, -2)
-            @test_throws DomainError delta(0.5, X, 2)
-            @test_throws DomainError delta(0.5, X, 1.0)
-        end
-
-    end
 
 end
 
